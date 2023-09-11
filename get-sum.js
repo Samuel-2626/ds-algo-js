d30dbb98af190629ddac5580eef6fcb2873d612d
Samuel Torimiro <efosonaname@gmail.com>
2023-09-11 12:20:11 -0500

    get sum

---
+++ get-sum.js
@@ -0,0 +1,10 @@
+// Write a function called getSum that takes in two numbers and returns the sum of those two numbers.
+
+function getSum(a, b) {
+    return a + b;
+}
+
+console.log(getSum(3, 6));
+
+
+
