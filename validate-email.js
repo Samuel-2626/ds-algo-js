5bed1381cba006a3a0bbe0c880d8ec9f8bab506a
Samuel Torimiro <efosonaname@gmail.com>
2023-09-27 12:49:27 -0500

    validate email

---
+++ validate-email.js
@@ -0,0 +1,12 @@
+// Write a function called validateEmail that takes in a string and returns whether 
+// the string is a valid email address. For the purposes of this challenge, a valid 
+// email address is defined as a string that contains an @ symbol and a . symbol.
+
+
+function validateEmail(email) {
+    const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+    return emailRegex.test(email);
+}
+
+
+console.log(validateEmail("test@gmail.com"));
