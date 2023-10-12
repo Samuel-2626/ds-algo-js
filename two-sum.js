99257952c12b25d4f042b0d22a2069d114bc6095
Samuel Torimiro <efosonaname@gmail.com>
2023-10-12 11:09:14 -0500

    two sum O(n)

--- two-sum.js
+++ two-sum.js
@@ -4,9 +4,22 @@
 
 function twoSum(arr, target) {
 
+    const numSet = new Set();
+
+
     for (let i = 0; i < arr.length; i++) {
-        console.log(arr[i]);
+
+        const complement = target - arr[i];
+
+        if (numSet.has(complement)) {
+            return [arr.indexOf(complement), i];
+        }
+
+        numSet.add(arr[i])
+
     }
+
+    return [];
 
 }
 
